bba942c6c785ec314ef6668f12e3247fa4cd920b
frank Nielson <f.nielson@gmail.com>
2023-11-11 10:43:35 -0700

    Added text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+    public event EventHandler OnSanityChanged;
     public static float DreamModeSpeedFactor = 0.75f;
 
 
