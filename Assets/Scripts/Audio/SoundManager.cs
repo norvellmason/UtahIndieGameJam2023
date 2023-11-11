221e1fbe76b849118f991bafab26bd8b5a7538ff
fnielson1 <f.nielson@gmail.com>
2023-11-11 16:21:47 -0700

    Walking sounds added. Has a small delay due to the audio

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -7,7 +7,7 @@ public class SoundManager : MonoBehaviour
     public static SoundManager Instance { get; private set; }
 
     private AudioSource _musicSource, _effectsSource;
-    private AudioClip _firedClip, _realWorldClip, _dreamWorldClip;
+    private AudioClip _realWorldClip, _dreamWorldClip, _firedClip, _realWalkClip, _dreamWalkClip;
 
 
     public static void Create()
@@ -34,18 +34,18 @@ public class SoundManager : MonoBehaviour
 
     private void LoadClips()
     {
-        if (_dreamWorldClip == null)
-            _dreamWorldClip = Resources.Load<AudioClip>("Audio/Music/Dark_Theme");
-        if (_realWorldClip == null)
-            _realWorldClip = Resources.Load<AudioClip>("Audio/Music/Dark_Theme");
+        _dreamWorldClip = Resources.Load<AudioClip>("Audio/Music/Dark_Theme");
+        _realWorldClip = Resources.Load<AudioClip>("Audio/Music/Dark_Theme");
 
-        if (_firedClip == null)
-            _firedClip = Resources.Load<AudioClip>("Audio/SFX/fired");
+        _realWalkClip = Resources.Load<AudioClip>("Audio/SFX/Walking-Light");
+        _dreamWalkClip = Resources.Load<AudioClip>("Audio/SFX/Walking-Dark");
+        _firedClip = Resources.Load<AudioClip>("Audio/SFX/fired");
     }
 
     private void PlaySound(AudioClip clip)
     {
-        _effectsSource.PlayOneShot(clip);
+        if (!_effectsSource.isPlaying)
+            _effectsSource.PlayOneShot(clip);
     }
 
     private void PlayMusic(AudioClip clip)
@@ -66,13 +66,30 @@ public class SoundManager : MonoBehaviour
         PlayMusic(_realWorldClip);
     }
 
+    public void PlayFired()
+    {
+        PlaySound(_firedClip);
+    }
+
     public void PlayDreamWorldMusic()
     {
         PlayMusic(_dreamWorldClip);
     }
 
-    public void PlayFired()
+    public void PlayWalk()
     {
-        PlaySound(_firedClip);
+        if (GameManager.Instance.IsRealWorld)
+            PlayRealWalk();
+        else
+            PlayDreamWalk();
+    }
+
+    private void PlayRealWalk()
+    {
+        PlaySound(_realWalkClip);
+    }
+    private void PlayDreamWalk()
+    {
+        PlaySound(_dreamWalkClip);
     }
 }
