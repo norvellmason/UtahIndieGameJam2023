61d8720a20c417d64de87ce2dd77dfbcf213d0d0
Mason Norvell <norvellmason@gmail.com>
2023-11-10 23:02:37 -0700

    Added player dream mode toggle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,24 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _Rigidbody;
     private Collider2D _Collider;
     private InputController _InputController;
-    public float MoveSpeed = 100;
-    public float JumpHeight = 1000;
+    private SpriteRenderer _SpriteRenderer;
+    private float RealMoveSpeed = 100;
+    private float DreamMoveSpeed = 13;
+    private float RealJumpHeight = 1500;
+    private float DreamJumpHeight = 600;
+
+    private bool _IsDreamMode = false;
+    private float _JumpCooldown = 0f;
+
+    private float GetMoveSpeed()
+    {
+        return _IsDreamMode ? DreamMoveSpeed : RealMoveSpeed;
+    }
+
+    private float GetJumpHeight()
+    {
+        return _IsDreamMode ? DreamJumpHeight : RealJumpHeight;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -16,34 +32,63 @@ public class PlayerController : MonoBehaviour
         _Rigidbody = gameObject.GetComponent<Rigidbody2D>();
         _Collider = gameObject.GetComponent<Collider2D>();
         _InputController = gameObject.GetComponent<InputController>();
+        _SpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleDreamMode();
+        }
+
+        if (_JumpCooldown > 0)
+        {
+            _JumpCooldown -= Time.deltaTime;
+        }
     }
 
     private void FixedUpdate()
     {
         if (_InputController.IsPressingRight && !_InputController.IsPressingLeft)
         {
-            _Rigidbody.AddForce(new Vector2(MoveSpeed, 0));
+            _Rigidbody.AddForce(new Vector2(GetMoveSpeed(), 0));
         }
 
         if(_InputController.IsPressingLeft && !_InputController.IsPressingRight)
         {
-            _Rigidbody.AddForce(new Vector2(-MoveSpeed, 0));
+            _Rigidbody.AddForce(new Vector2(-GetMoveSpeed(), 0));
         }
 
-        if (_InputController.IsPressingJump && IsGrounded())
+        if (_InputController.IsPressingJump && IsGrounded() && _JumpCooldown <= 0)
         {
-            _Rigidbody.AddForce(new Vector2(0, JumpHeight));
+            _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, 0);
+            _Rigidbody.AddForce(new Vector2(0, GetJumpHeight()));
+            _JumpCooldown = 0.5f;
         }
     }
 
     private bool IsGrounded()
     {
-        return Physics2D.Raycast(transform.position, Vector2.down, _Collider.bounds.extents.y + 0.2f);
+        return Physics2D.Raycast(transform.position, Vector2.down, _Collider.bounds.extents.y + 0.1f);
+    }
+
+    private void ToggleDreamMode()
+    {
+        _IsDreamMode = !_IsDreamMode;
+
+        if (_IsDreamMode)
+        {
+            _Rigidbody.gravityScale = 0.75f;
+            _Rigidbody.drag = 3;
+            _SpriteRenderer.color = Color.black;
+        }
+        else
+        {
+            _Rigidbody.gravityScale = 8;
+            _Rigidbody.drag = 10;
+            _SpriteRenderer.color = Color.white;
+        }
     }
 }
